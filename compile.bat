d8b262c9f5d0f8228dc350a3830ef0dd3695f36a
Frederico Rappa <frerappa@gmail.com>
2023-05-07 11:35:54 -0300

    Added step heap

--- compile.bat
+++ compile.bat
@@ -8,9 +8,27 @@ cd distributor
 dnc . -sp ../server -v
 dnc RemoteDist.dn
 dnc RemoteList.dn
+dnc ./monitoring -sp ../server -v
+dnc ./util  -v
+dnc ./proxy -v
+
+
 cd ..
 
 @REM  Compile client
 cd client
 dnc . -v
+cd ..
+
+cd readn
+dnc . -v
+cd ../readn-writen
+dnc . -v
+cd ../writen
+dnc . -v
+cd ../constant
+dnc . -v
+cd ../step
+dnc . -v
+
 cd ..
