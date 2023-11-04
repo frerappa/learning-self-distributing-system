414e3bbfb6fdbb7e4a36fc52f47ba3ec26104d98
Frederico Rappa <frerappa@gmail.com>
2023-11-04 14:49:11 -0300

    Refactored learning distributor

--- learning-distributor/app.py
+++ learning-distributor/app.py
@@ -2,9 +2,11 @@ import time
 from clients.DistributorClient import DistributorClient
 
 OBSERVATION_WINDOW = 7.5  # in seconds
+MODEL = "UCB"
 
 if __name__ == '__main__':
-    client = DistributorClient("UCB")
+    print(f"Server started with model {MODEL} and observation window {OBSERVATION_WINDOW} seconds")
+    client = DistributorClient(MODEL)
     client.init_model()
 
     while True:
--- learning-distributor/clients/DistributorClient.py
+++ learning-distributor/clients/DistributorClient.py
@@ -1,4 +1,5 @@
-from models import UCB, BaseBanditModel
+from models.UCB import *
+from models.BaseBanditModel import *
 import requests
 
 
@@ -9,35 +10,38 @@ class DistributorClient:
     def __init__(self, type: str):
         self.model: BaseBanditModel = None
         self.model_type = type
+        self.should_reconfigure: bool = True
 
     def _calculate_reward(self, average_response_time: float) -> float:
         return 100 / average_response_time
 
     def init_model(self):
         if self.model_type == "UCB":
-            response = requests.get("{}:{}/ucb/init".format(base_url, base_port))
-            # call api
-            self.model: UCB = UCB(10)  # todo change
-        else:
-            return
+            response = requests.get(f"{DistributorClient.base_url}:{DistributorClient.base_port}/ucb/init")
+            arms = int(response.content.decode('utf-8'))
+            print(f"Initializing UCB1 model with {arms} arms")
+            self.model: UCB = UCB(arms)  # todo change
 
     def update_reward(self):
         if self.model_type == "UCB":
-            response = requests.get("{}:{}/ucb/perception-data".format(base_url, base_port))
-            # should return current index and avg response time
-            # call api
+            response = requests.get(f"{DistributorClient.base_url}:{DistributorClient.base_port}/ucb/perception-data")
+            if (response.content == "NOT FOUND"):
+                self.should_reconfigure = False
+                return
+            self.should_reconfigure = True
+            action, average_response_time = response.content.decode('utf-8').split("|")
+            reward = self._calculate_reward(float(average_response_time))
+            print(f"Reward value {reward} for action {action} with average response time {average_response_time}")
             self.model.update(
-                10,  # action
-                10.0  # reward
+                int(action),
+                reward
             )
-        else:
-            return
 
     def choose_index(self):
         if self.model_type == "UCB":
+            if not self.should_reconfigure:
+                return
+
             next_composition = self.model.choose_composition_index()
-
-            requests.post("{}:{}/ucb/composition".format(base_url, base_port), data=str(next_composition))
-
-        else:
-            return
+            print(f"Pulling arm {next_composition}")
+            requests.post(f"{DistributorClient.base_url}:{DistributorClient.base_port}/ucb/composition", data=str(next_composition))
